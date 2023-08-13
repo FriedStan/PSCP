3f1a1354e470eb908dcabe949385b1340095b8d1
KuhnphOn <88961962+KuhnphOn@users.noreply.github.com>
2023-08-13 13:45:09 +0700

    Edit second_converter.py

--- second_converter.py
+++ second_converter.py
@@ -7,3 +7,12 @@ def inception():
     one_min = int(input())# 1-min = input sec
     one_hour = int(input())# 1-hour = input min
     one_day = int(input()) # 1-day = input hour
+    minute = k_sec // one_min
+    sec = k_sec % one_min
+    hour = minute // one_hour
+    minute = minute % one_hour
+    hour = hour % one_day
+    print("{:d}:{:d}:{:d}".format(hour, minute, sec))
+
+
+inception()
