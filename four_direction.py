d04a7ff6fdf07ee6a87087ffef229e52e726ca43
FriedStan <66070030@kmitl.ac.th>
2023-08-14 14:00:51 +0700

    Update four_direction

--- four_direction.py
+++ four_direction.py
@@ -1,30 +1,80 @@
 """FourDirections"""
 
 
-def up_down_arrow(row, direction):
+def left_arrow(row):
     """Make up arrow"""
-    if direction == "U":
-        if row == 0:
-            text = "  *  "
-        elif row == 1:
-            text = " *** "
-        elif row == 2:
-            pass
+    if row == 0:
+        text = "  *  "
+    elif row == 1:
+        text = " *   "
+    elif row == 2:
+        text = "*****"
+    elif row == 3:
+        text = " *   "
+    elif row == 4:
+        text = "  *  "
+    return text
+
+
+def right_arrow(row):
+    """Make up arrow"""
+    if row == 0:
+        text = "  *  "
+    elif row == 1:
+        text = "   * "
+    elif row == 2:
+        text = "*****"
+    elif row == 3:
+        text = "   * "
+    elif row == 4:
+        text = "  *  "
+    return text
+
+
+def up_arrow(row):
+    """Make up arrow"""
+    if row == 0:
+        text = "  *  "
+    elif row == 1:
+        text = " *** "
+    elif row == 2:
+        text = "* * *"
+    elif row == 3:
+        text = "  *  "
+    elif row == 4:
+        text = "  *  "
+    return text
+
+
+def down_arrow(row):
+    """Make up arrow"""
+    if row == 0:
+        text = "  *  "
+    elif row == 1:
+        text = "  *  "
+    elif row == 2:
+        text = "* * *"
+    elif row == 3:
+        text = " *** "
+    elif row == 4:
+        text = "  *  "
+    return text
 
 
 def four_arrow(seq):
     """Point arrow"""
     text = ""
     for row in range(5):
-        for _ in range(len(seq)):
-            count = 0
-            for char in seq:
-                if char in ("U", "D"):
-                    if row in (0, 3, 4):
-                        text += "*"
-                else:
-                    text += " "
-                count += 1
+        for char in seq:
+            if char == "U":
+                text += up_arrow(row)
+            elif char == "D":
+                text += down_arrow(row)
+            elif char == "L":
+                text += left_arrow(row)
+            elif char == "R":
+                text += right_arrow(row)
+            text += " "
         text += "\n"
     print(text)
 
