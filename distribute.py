c80d613135e6700636332ce1a5c353f98bf36a16
FriedStan <66070030@kmitl.ac.th>
2023-09-14 16:52:40 +0700

    Add distribute

---
+++ distribute.py
@@ -0,0 +1,21 @@
+"""[Midterm 2023] Distribute"""
+
+
+def distribute(money, child):
+    """[Midterm 2023] Distribute"""
+    if money >= child:
+        money -= child
+        successfull_8 = min(money // 7, child)
+        money -= successfull_8 * 7
+        if money != 0:
+            not_8 = child - successfull_8
+            if money == 3 and not_8 == 1:
+                successfull_8 -= 1
+            if not_8 == 0 and money != 0:
+                successfull_8 -= 1
+        print(successfull_8)
+    else:
+        print(-1)
+
+
+distribute(int(input()), int(input()))
