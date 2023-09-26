7a3536c8a4631ba695300656979c5d1bff07a3c8
FriedStan <66070030@kmitl.ac.th>
2023-09-26 22:42:25 +0700

    Add calculator v2

--- calculator_v2.py
+++ calculator_v2.py
@@ -4,7 +4,15 @@
 def calc_button(num):
     """Count each time the button is placed"""
     if num != 1:
-        pass
+        digit = len(str(num))
+        button_pressed = 0
+        number_left = num
+        for i in range(digit - 1):
+            that_much = 9 * (10 ** i)
+            button_pressed += ((that_much * (i + 2)))
+            number_left -= that_much
+        button_pressed += number_left * (digit + 1)
+        num = button_pressed
     print(num)
 
 calc_button(int(input()))
