14ed03ce1f8c330515fcc7f57dbe9a6438f99d5f
FriedStan <66070030@kmitl.ac.th>
2023-10-09 15:22:14 +0700

    Re gcdV1

--- gcd_v1.py
+++ gcd_v1.py
@@ -3,12 +3,10 @@
 
 def find_gcd(num_1, num_2):
     """Find the GCD of 2 numbers"""
-    maxine = 0
+    maxine = max(num_1, num_2)
     for num in range(1, min(num_1, num_2) + 1):
         if num_1 % num == num_2 % num == 0:
             maxine = num
-    if 0 in (num_1, num_2):
-        maxine = max(num_1, num_2)
     print(maxine)
 
 
