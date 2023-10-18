e9846f073f2ccfe184baa3e1c19eee8237b1208d
FriedStan <66070030@kmitl.ac.th>
2023-10-18 09:42:09 +0700

    Fix compound interest

--- compound_interest.py
+++ compound_interest.py
@@ -3,8 +3,12 @@
 
 def compound_calc(money, interest, year):
     """Calculate the compound interest"""
-    final = money * (interest ** year)
-    print("{}".format(str(final)))
+    try:
+        final = money * (interest ** year)
+    except OverflowError:
+        interest /= 10
+        final = money * (interest ** year)
+    print("{:.2f}".format(final))
 
 
-compound_calc(int(input()), (100 + float(input())) / 100, int(input()))
+compound_calc(int(input()), 1 + (float(input()) / 100), int(input()))
